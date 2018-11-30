72e7a92615e9fe9ea5c162e41dab11a04c631bad
梁彬 <liangbin@sharinglearn.com>
2018-11-30 13:22:17 +0800

    类ReadWriteChineseTxt的主要目的在于从 DataInFile 中读取数据，并且按行直接写入log.txt文件中，但写入的时候遇到了问题。貌似可以通过 while 循环把 DataInfile 中的每一行数据都读到，但是写入到 log文件时总是不能写完。
    我在 writeLog 这个函数中加上了行号+文字内容的打印，确实也发现 log 写到1000多行就不会写入了，并且一般 log 的最后一行的内容还不完整。
    
    实在想不明白这个问题是怎么导致的。看了一下也好像没有特殊字符。
    
    先把状态提交了再说。期待高手指路。

--- ReadWriteChineseTxt.java
+++ ReadWriteChineseTxt.java
@@ -2,49 +2,153 @@ import java.io.*;
 
 public class ReadWriteChineseTxt
 {  
+	File inFile;
+	File dataInfile, dataOutFile, logFile;
+	BufferedWriter logWriter;
+	int logLineCount=0;
+	
+	class LineEmptyException extends Exception
+	{
+		String ErrorMessage;
+		public LineEmptyException(String ErrMsg)
+		{
+			ErrorMessage= ErrMsg;
+		}
+		public String getMessage()
+		{
+			return this.ErrorMessage;
+		}
+	}
+	
+	public ReadWriteChineseTxt(String configureFilePath)
+	{
+		BufferedReader cfgReader=null;
+		inFile= new File(configureFilePath);
+		
+		try {
+			cfgReader= new BufferedReader(new InputStreamReader(new FileInputStream(inFile)));
+		}catch (FileNotFoundException e) 
+        {  
+            System.out.println("configuration file is not found.");  
+        }
+		
+		String firstLine="", secondLine="", thirdLine="";
+		
+		try {
+			firstLine= cfgReader.readLine();
+			if(firstLine==null) throw new LineEmptyException("Error: first empty line read!");
+		}catch(LineEmptyException lee) {
+			System.out.println(lee.getMessage());
+		}catch (IOException e) 
+        {  
+            System.out.println("Read Exceptioned");  
+        }
+		
+		this.dataInfile= new File(this.ExtractConfigureFileName(firstLine));
+		
+		try {
+			secondLine= cfgReader.readLine();
+			if(secondLine==null) throw new LineEmptyException("Error: second empty line read!");
+		}catch(LineEmptyException lee) {
+			System.out.println(lee.getMessage());
+		}catch (IOException e) 
+        {  
+            System.out.println("Read Exceptioned");  
+        }
+		
+		this.dataOutFile= new File(this.ExtractConfigureFileName(secondLine));
+		
+		try {
+			thirdLine= cfgReader.readLine();
+			if(thirdLine==null) throw new LineEmptyException("Error: third empty line read!");
+		}catch(LineEmptyException lee) {
+			System.out.println(lee.getMessage());
+		}catch (IOException e) 
+        {  
+            System.out.println("Read Exceptioned");  
+        }
+		
+		this.logFile= new File(this.ExtractConfigureFileName(thirdLine));
+		
+		try{
+			this.logWriter= new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logFile)));
+		}catch (FileNotFoundException e) 
+        {  
+            System.out.println("log file is not fond");  
+        } 
+		
+	}
+	
+	String ExtractConfigureFileName(String cfgLine)
+	{
+		String fileName="";
+		fileName= cfgLine.substring(cfgLine.indexOf("=")+1);
+		return fileName;
+	}
+	
+	void writeLog(String logMsg)
+	{
+		try {
+			this.logWriter.write(logMsg+this.logLineCount+"\r\n");
+			this.logLineCount++;
+			if(this.logLineCount==1133)
+			{
+				System.out.println("Reaching edge point");
+			}
+			if(this.logLineCount>1133)
+				System.out.println("log written "+ this.logLineCount+": "+logMsg+"\r\n");
+			if(this.logLineCount%10==0)
+				System.out.println("Log line Count"+this.logLineCount);
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		return;
+	}
+	
 	public static void main(String [] args)
 	{  
-        File firstFile = new File("C://tmp//fileone.txt");  
-        File secondFile= new File("C://tmp//filesecond.txt");  
-        BufferedReader in = null;  
-        BufferedWriter out = null;        
-        try 
-        {          
-            in = new BufferedReader(new InputStreamReader(new FileInputStream(firstFile), "gbk"));  
-            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(secondFile), "gbk"));  
-            String line = "";  
-            while((line = in.readLine())!=null)
-            {  
-                System.out.println(line);  
-                out.write(line+"\r\n");  
-            }  
-        } 
-        catch (FileNotFoundException e) 
-        {  
+		ReadWriteChineseTxt ins= new ReadWriteChineseTxt("configure.txt");
+		
+		BufferedReader dataReader=null;
+		BufferedWriter dataWriter=null;
+		
+		try {
+			dataReader = new BufferedReader(new InputStreamReader(new FileInputStream(ins.dataInfile)));
+			dataWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ins.dataOutFile)));
+			
+			String line="";
+			int lineCount=0;
+			
+			while((line= dataReader.readLine())!=null )
+			{
+				ins.writeLog(line);
+				lineCount++;
+				if(lineCount%10==0)
+					System.out.println("line count: "+ lineCount);
+			}
+			System.out.println("final line count: "+ lineCount);
+			
+		}catch (FileNotFoundException e){  
             System.out.println("file is not fond");  
-        } 
-        catch (IOException e) 
-        {  
+        }catch (IOException e){  
             System.out.println("Read or write Exceptioned");  
+        }finally {
+        	if(null!= dataReader)
+        	{
+        		try {
+        			dataReader.close();
+        		}catch(IOException ioe) {
+        			ioe.printStackTrace();
+        		}
+        	}
+        	if(null!= dataWriter)
+        	{
+        		try {
+        			dataWriter.close();
+        		}catch(IOException ioe) {
+        			ioe.printStackTrace();
+        		}
+        	}
         }
-        finally
-        {             
-            if(null!=in)
-            {   
-                try {  
-                    in.close();  
-                } catch (IOException e) {  
-                    e.printStackTrace();  
-                }
-            }  
-            if(null!=out)
-            {  
-                try {  
-                    out.close();  
-                } catch (IOException e) {  
-                    e.printStackTrace();  
-                }
-            }
-        }
-    }  
+	}
 }  
